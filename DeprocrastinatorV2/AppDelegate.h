9126baf8adf2149706b2ea97a1149799544422dc
KacheMoney <basel.farag@gmail.com>
2014-09-27 20:03:38 -0500

    First Commit

---
+++ DeprocrastinatorV2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeprocrastinatorV2
+//
+//  Created by Basel Farag on 9/27/14.
+//  Copyright (c) 2014 NextTierGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeprocrastinatorV2/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  DeprocrastinatorV2
+//
+//  Created by Basel Farag on 9/27/14.
+//  Copyright (c) 2014 NextTierGames. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UITableViewDelegate>
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 3;
+}
+
+@end
